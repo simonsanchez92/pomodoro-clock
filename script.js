13ac7f1a205d00408ad7c54784d0eb9a6c2c944f
simonsanchez92 <simonsanchez.1992@gmail.com>
2020-12-05 00:01:41 -0300

    fixing clock accuracy

--- script.js
+++ script.js
@@ -14,16 +14,22 @@ const stop = document.getElementById('stop');
 
 
 let deadline = new Date(Date.now() + 1000 * 10);
+let remaining = 1000 *  10;
 
-const getRemainingTime = ()=>{
-    const milisecondsLeft = deadline - new Date(Date.now());
+let perf =  performance.now();
+
+
+const getRemainingTime = (remainder)=>{
+    // const milisecondsLeft = deadline - new Date(Date.now());
+
+    const milisecondsLeft = new Date(Date.now() + remainder) - new Date(Date.now());
     const seconds = Math.floor((milisecondsLeft/1000) % 60);
     const minutes = Math.floor( (milisecondsLeft/1000/60) % 60 );
 
     return {minutes, seconds, milisecondsLeft}
 }
 
-
+console.log(getRemainingTime(remaining));
 
 let timeInterval;
 const startClock = ()=>{
@@ -31,19 +37,23 @@ const startClock = ()=>{
     title.innerText = 'Focus!';
 
    // Initialize the clock - avoid delay
-    const time = getRemainingTime();
+    const time = getRemainingTime(remaining);
+    
     timer.innerHTML = `${time.minutes < 10 ? '0' + time.minutes : time.minutes}:${time.seconds < 10 ?  '0'+ time.seconds  : time.seconds}`;
 
 
     timeInterval = setInterval(() => {
+     
+        remaining-= 1000;
 
-        const time = getRemainingTime();
+        const time = getRemainingTime(remaining);
         timer.innerHTML = `${time.minutes < 10 ? '0' + time.minutes : time.minutes}:${time.seconds < 10 ?  '0'+ time.seconds  : time.seconds}`;
+        
         console.log(time)
 
         if (time.milisecondsLeft < 0) {
           clearInterval(timeInterval);
-          timer.innerHTML = `00:09`;
+        //   timer.innerHTML = `00:09`;
           rest();
           }
     }, 1000);
@@ -53,18 +63,20 @@ const startClock = ()=>{
 const rest = ()=>{
     title.innerText = 'Rest!';
     
-    deadline = new Date(Date.now() + 1000 * 10);
-
+    // deadline = new Date(Date.now() + 1000 * 10);
+    remaining = 1000 * 10;
     timeInterval = setInterval(() => {
+        
+        remaining-= 1000;
 
-        const time = getRemainingTime();
+        const time = getRemainingTime(remaining);
         timer.innerHTML = `${time.minutes < 10 ? '0' + time.minutes : time.minutes}:${time.seconds < 10 ?  '0'+ time.seconds  : time.seconds}`;
      
         if (time.milisecondsLeft < 0) {
           clearInterval(timeInterval);
-          timer.innerHTML = `00:09`;
-          deadline = new Date(Date.now() + 1000 * 10);
-
+        //   timer.innerHTML = `00:09`;
+        //   deadline = new Date(Date.now() + 1000 * 10);
+            remaining = 1000 * 10;
           startClock();
           }
     }, 1000);
@@ -75,7 +87,7 @@ stop.addEventListener('click', ()=>{
     timer.innerHTML = `25:00`;
     title.innerText = 'Ready!';
 
-    deadline = new Date(Date.now() + 1000 * 60 * 25);
+    // deadline = new Date(Date.now() + 1000 * 60 * 25);
 })
 
 
