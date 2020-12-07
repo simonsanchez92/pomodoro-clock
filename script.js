d6f43a72db70ee09098ab91f2636278524288796
simonsanchez92 <simonsanchez.1992@gmail.com>
2020-12-07 13:09:47 -0300

    long rest functionality added

--- script.js
+++ script.js
@@ -4,17 +4,22 @@
 const timer = document.getElementById('timer');
 const title = document.getElementById('clock-title');
 
+const clockContainer = document.getElementById('clock-container');
+
 //Controls
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 
+//Tomato chart
+const tomatoes = document.getElementById('tomatoes-container');
+
+const sessions = document.getElementById('session-count');
 
 
 
 
-
-let deadline = new Date(Date.now() + 1000 * 10);
-let remaining = 1000 *  10;
+let remaining = 1000 * 5;
+let sessionCount = 0;
 
 
 
@@ -35,7 +40,7 @@ let timeInterval;
 const startClock = ()=>{
 
     title.innerText = 'Focus!';
-
+    clockContainer.classList.add('animate');
    // Initialize the clock - avoid delay
     const time = getRemainingTime(remaining);
     
@@ -54,6 +59,12 @@ const startClock = ()=>{
         if (time.milisecondsLeft < 0) {
           clearInterval(timeInterval);
         //   timer.innerHTML = `00:09`;
+      clockContainer.classList.remove('animate');
+
+          sessionCount++;
+          
+          addTomato();
+          
           rest();
           }
     }, 1000);
@@ -61,33 +72,94 @@ const startClock = ()=>{
 }
 
 const rest = ()=>{
-    title.innerText = 'Rest!';
-    
-    // deadline = new Date(Date.now() + 1000 * 10);
-    remaining = 1000 * 10;
-    timeInterval = setInterval(() => {
-        
-        remaining-= 1000;
 
-        const time = getRemainingTime(remaining);
-        timer.innerHTML = `${time.minutes < 10 ? '0' + time.minutes : time.minutes}:${time.seconds < 10 ?  '0'+ time.seconds  : time.seconds}`;
-     
-        if (time.milisecondsLeft < 0) {
-          clearInterval(timeInterval);
-        //   timer.innerHTML = `00:09`;
-        //   deadline = new Date(Date.now() + 1000 * 10);
-            remaining = 1000 * 10;
-          startClock();
-          }
-    }, 1000);
+    if(sessionCount === 4){
+      longPause();
+
+    }else{
+      title.innerText = 'Rest!';
+    
+      // deadline = new Date(Date.now() + 1000 * 10);
+      remaining = 1000 * 5;
+      timeInterval = setInterval(() => {
+          
+          remaining-= 1000;
+  
+          const time = getRemainingTime(remaining);
+          timer.innerHTML = `${time.minutes < 10 ? '0' + time.minutes : time.minutes}:${time.seconds < 10 ?  '0'+ time.seconds  : time.seconds}`;
+       
+          if (time.milisecondsLeft < 0) {
+            clearInterval(timeInterval);
+         
+              remaining = 1000 * 5;
+              if(sessionCount < 4){
+                startClock();
+              }
+            
+            }
+      }, 1000);
+    }
+
+    
+}
+
+const longPause = ()=>{
+  remaining = 1000 * 15;
+ 
+  clockContainer.classList.add('long-rest');
+
+  title.innerText = 'Long rest!';
+
+  timeInterval = setInterval(() => {
+        
+    remaining-= 1000;
+
+    const time = getRemainingTime(remaining);
+    timer.innerHTML = `${time.minutes < 10 ? '0' + time.minutes : time.minutes}:${time.seconds < 10 ?  '0'+ time.seconds  : time.seconds}`;
+ 
+    if (time.milisecondsLeft < 0) {
+      clearInterval(timeInterval);
+      
+      clockContainer.classList.remove('long-rest');
+
+       remaining = 1000 * 5;
+       sessionCount = 0;
+       tomatoes.innerHTML = '';
+       sessions.innerText = `${sessionCount}/4`;
+      startClock();
+
+      }
+}, 1000);
+}
+
+const addTomato = ()=>{
+
+
+  if(sessionCount <= 4){
+    sessions.innerText = `${sessionCount}/4`;
+
+    const newTomato = document.createElement('div');
+    newTomato.classList.add('tomato');
+    newTomato.innerHTML = `<img src='./img/tomato.png'/>`
+    tomatoes.appendChild(newTomato);
+
+  
+  }
+ 
 }
 
 stop.addEventListener('click', ()=>{
     clearInterval(timeInterval);
     timer.innerHTML = `25:00`;
-    title.innerText = 'Ready!';
+    title.innerText = 'Ready?';
 
-    // deadline = new Date(Date.now() + 1000 * 60 * 25);
+    //Reset all values
+    sessionCount = 0;
+    sessions.innerText = `${sessionCount}/4`;
+    tomatoes.innerHTML = '';
+    clockContainer.classList.remove('animate');
+    
+
 })
 
 
